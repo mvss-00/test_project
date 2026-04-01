9d98354acf34b042f38d356a1844a709111661eb
dnlmvss <danielmovses18@gmail.com>
2026-04-01 12:40:24 -0700

    time converter

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
 "use strict"
 
-alert("How are u")
+function getTimeFromMinutes(min) {
+    let ost = min % 60,
+        cel = Math.floor(min / 60),
+        hoursStr = '';
+
+    if (typeof(min) !== 'number' || !Number.isInteger(min) || min < 0) {
+        return 'Введите количество минут';
+    } else if (min <= 59) {
+        return `Это ${ost} минут`
+    } else {
+        switch (cel) {
+            case 1:
+                hoursStr = 'час';
+                break;
+            case 2:
+                hoursStr = 'часа';
+                break;
+            case 3:
+                hoursStr = 'часа';
+                break;
+            case 4:
+                hoursStr = 'часа';
+                break;
+            default:
+                hoursStr = 'часов';
+           }
+        return `Это ${cel} ${hoursStr} ${ost} минут`
+    }
+}
+console.log(getTimeFromMinutes());
